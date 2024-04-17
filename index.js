f1405d2637786643f671e537d6144e0553ab1b4f
bashartech <144168218+bashartech@users.noreply.github.com>
2024-04-18 01:12:14 +0500

    Add files via upload

---
+++ index.js
@@ -0,0 +1,13 @@
+// Question 7
+console.log(2 + 6);
+console.log(9 - 1);
+console.log(4 * 2);
+console.log(16 / 2);
+// Question 8
+console.log(5 + 3);
+console.log(5 + 3);
+console.log(5 + 3);
+console.log(5 + 3);
+// Question 9
+var fav_num = 56;
+console.log("My favourite number is ".concat(fav_num));
---
+++ index.ts
@@ -0,0 +1,24 @@
+// DAY 3
+
+// Question 7
+console.log(2+6);
+console.log(9-1);
+console.log(4*2);
+console.log(16/2);
+
+
+// Question 8
+
+console.log(5+3)
+console.log(5+3)
+console.log(5+3)
+console.log(5+3)
+
+// Question 9
+
+let fav_num = 56;
+console.log(`My favourite number is ${fav_num}.`)
+
+
+
+
